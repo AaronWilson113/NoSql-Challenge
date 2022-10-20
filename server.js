734a984dd23d3c8e1f41085cead843ffe24b89a9
President <aaron.m.wilson@uconn.edu>
2022-10-20 19:08:24 -0400

    server troubleshooting

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const db = require('./config/connection')
+const routes = require('./routes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api' , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on ${PORT}`)
   });
-
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+});
